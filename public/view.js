fc662a76d8590684661298a9882569f5c13ba432
Jason Finney <finney.jason.218@gmail.com>
2018-09-20 08:38:57 -0500

    got the routes!

--- public/view.js
+++ public/view.js
@@ -1,4 +1,67 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+    var burgerUneatenList = $("<li>");
+    var burgerEatenList = $("<li>");
 
+
+
+    $(".uneatenBurgers").push(burgerUneatenList)
+
+    var uneatenBurgersArray = [];
+    var eatenBurgersArray = [];
+
+    getUneatenBurgers();
+    getEatenBurgers();
+
+    function getUneatenBurgers() {
+        $.ajax({
+            method: "GET",
+            url: "/api/burgers",
+            data: burgers
+        }).then(function (data) {
+
+        });
+    };
+
+    function getEatenBurgers() {
+        $.get("/api/burgers", function (data) {
+
+        });
+    };
+
+    $(document).on("click", ".devourburger", function (event) {
+        event.preventDefault();
+        var id = $(this).data("burgerid");
+        var newDevouredState = {
+            devoured: true
+        };
+        $.ajax("/api/burgers/" + id, {
+            method: "PUT",
+            data: newDevouredState
+        }).then(function () {
+            location.reload();
+        });
+    });
+    $("#createBurger").on("submit", function (event) {
+        event.preventDefault();
+        var newBurger = {
+            burger: $("#createBurger [name=Burger]").val().trim(),
+            devoured: false
+        };
+        console.log(newBurger);
+        $.ajax("/api/burgers", {
+            method: "POST",
+            data: newBurger
+        }).then(function () {
+            location.reload();
+        });
+    });
+    $(document).on("click", ".delete-burger", function (event) {
+        var id = $(this).data("burgerid");
+        event.preventDefault();
+        $.ajax("/api/burgers/" + id, {
+            method: "DELETE"
+        }).then(function () {
+            location.reload();
+        });
+    });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,7 @@ var db = require("../models");
 module.exports = function (app) {
     app.get("/", function (req, res) {
         db.Burger.findAll({}).then(function (results) {
+            console.log(results);
             res.json(results);
         });
     });
